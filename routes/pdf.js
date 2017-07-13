cb0e6bdb88131b5bd3836744bff959944b8a2bf4
James Homer <jameshomer85@gmail.com>
2017-07-13 14:44:31 +0100

    fix pdf

--- routes/pdf.js
+++ routes/pdf.js
@@ -2,10 +2,8 @@ const Pdf = require('ace-api/lib/pdf');
 const Helpers = require('ace-api/lib/helpers');
 
 module.exports = (util, config) => {
-  const pdfUrl = `${config.assist.url}/pdf/download`;
-
   util.router.get('/pdf/view/:template?/:id?.:ext?', (req, res) => {
-    const pdf = new Pdf(util.getConfig(config, req).pdf.templates, pdfUrl);
+    const pdf = new Pdf(util.getConfig(config, req));
 
     pdf.getPayload(req.params.template || req.query.template, req.params.id || req.query.id)
       .then((payload) => {
@@ -20,7 +18,7 @@ module.exports = (util, config) => {
   });
 
   util.router.get('/pdf/download/:template?/:id?.:ext?', (req, res) => {
-    const pdf = new Pdf(util.getConfig(config, req).pdf.templates, pdfUrl);
+    const pdf = new Pdf(util.getConfig(config, req));
 
     pdf.getPayload(req.params.template || req.query.template, req.params.id || req.query.id)
       .then((payload) => {
@@ -35,7 +33,7 @@ module.exports = (util, config) => {
   });
 
   util.router.get('/pdf/payload/:template?/:id?.:ext?', (req, res) => {
-    const pdf = new Pdf(util.getConfig(config, req).pdf.templates, pdfUrl);
+    const pdf = new Pdf(util.getConfig(config, req));
 
     pdf.getPayload(req.params.template || req.query.template, req.params.id || req.query.id)
       .then((payload) => {
@@ -45,7 +43,7 @@ module.exports = (util, config) => {
   });
 
   util.router.get('/pdf/:template?/:id?.:ext?', (req, res) => {
-    const pdf = new Pdf(util.getConfig(config, req).pdf.templates, pdfUrl);
+    const pdf = new Pdf(util.getConfig(config, req));
 
     pdf.getPayload(req.params.template || req.query.template, req.params.id || req.query.id)
       .then((payload) => {
