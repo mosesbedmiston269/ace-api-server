732b95754ae6fe3df3f63ae0d06add68c701bf27
James Homer <jameshomer85@gmail.com>
2018-01-10 19:44:24 +0000

    inspect

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'change_me';
 
 const express = require('express');
 const http = require('http');
